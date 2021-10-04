50939dfc23ee24b2e3f4320775164fe3faeb8fde
Johnny William <50759662+Jownao@users.noreply.github.com>
2021-10-03 22:44:05 -0300

    Adaptar menu

--- main.py
+++ main.py
@@ -4,28 +4,24 @@ from automato import Automato
 menu()
 
 while True:
-    resposta = int(input(f'1 - Cadastrar autômato\n'+
-                        f'2 - Imprimir autômato\n'+
-                        f'3 - Transformar autômato\n'+
-                        f'4 - Testar palavra\n'+
-                        f'0 - Finalizar progama\n\n'))
-    
-    if resposta == 1:
-        sub_menu("Cadastrar autômato")
-        nome = input("Digite o nome do arquivo, sem o\".txt\": ")
-        automato = Automato.ler_automato(nome)
+    resposta = int(input('\n1 - Cadastrar autômato\n' +
+                         '2 - Testar palavra\n' +
+                         '0 - Finalizar progama\n\nOpção: '))
+    if 0 <= resposta <= 2:
+        if resposta == 1:
+            sub_menu("Cadastrar autômato")
+            nome = input("Digite o nome do arquivo, sem o\".txt\": ")
+            automato = Automato(nome)
 
-    elif resposta == 2:
-        sub_menu("Imprimir autômato")
-        Automato.imprimir(automato)
+        elif resposta == 2:
+            sub_menu("Testar palavra")
+            palavra = input("Digite a palavra: ")
+            print()
+            automato.testar_palavra(palavra)
 
-    elif resposta == 3:
-        sub_menu("Transformar autômato")
-
-    elif resposta == 4:
-        sub_menu("Testar palavra")
-
-    elif resposta ==0:
-        sub_menu("Finalizar progama")
-        encerrar()
-        break
+        else:
+            sub_menu("Finalizar progama")
+            encerrar()
+            break
+    else:
+        print("Opção inválida, por favor, tente novamente!")
